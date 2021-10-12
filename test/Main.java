2b896d5d00c05cf823601b375c784ae7586825e1
dakhtar1 <akhtardaanyal@gmail.com>
2021-10-12 19:27:31 -0400

    Comment

--- test/Main.java
+++ test/Main.java
@@ -3,6 +3,7 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
 
+//Compile this class
 public class Main {
     public static void main(String[] args) throws Exception {
         RecipeAO recipeAOCSV = new RecipeAOCSV("../src/data/recipes.csv");
