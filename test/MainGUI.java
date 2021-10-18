04dee299dfeed20f1470472ab6dd30ad4f79e241
camilohv <72526400+camilohv@users.noreply.github.com>
2021-10-18 10:01:21 -0400

    created nav bar

--- test/MainGUI.java
+++ test/MainGUI.java
@@ -1,9 +1,7 @@
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import java.util.*;
+import javax.swing.*;
+import java.util.List;
+import java.awt.*;
+
 public class MainGUI {
     public static void main(String[] args) throws Exception {
         RecipeAO recipeAOCSV = new RecipeAOCSV("src/data/recipes.csv");
@@ -16,13 +14,22 @@ public class MainGUI {
         Menu deleteRecipeMenu = new DeleteRecipeMenu(recipeAOCSV);
 
         DrawPanel p = new DrawPanel();
+
         JFrame f = new JFrame("Recipe Book");
-        f.add(p);
+        JPanel content = new JPanel();
+        JPanel explore = new JPanel();
+        JPanel retrieve = new JPanel();
+        JPanel create = new JPanel();
+        JPanel delete = new JPanel();
+        JPanel all = new JPanel();
+
+        //f.add(p);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(1000, 800);
         f.setVisible(true);
         JTextArea textWelcome = new JTextArea("Welcome to the Recipe Book", 200, 200);
 
+        /*
         exploreRecipesMenu.show();
         JLabel title = new JLabel("Recipe Name");
         title.setBounds(400,100,80,25);
@@ -34,7 +41,7 @@ public class MainGUI {
             yPos += 50;
             name.setBounds(400,yPos,80,25);
             p.add(name);
-        }
+        }*/
 
 
         //Sizing and placing buttons on the panel
@@ -48,6 +55,26 @@ public class MainGUI {
         btnCreate.setBounds(25,500,150,40);
         JButton btnDelete = new JButton("Delete");
         btnDelete.setBounds(25, 650, 150, 40);
+
+        CardLayout cl = new CardLayout(3,1);
+        GridLayout gl = new GridLayout(3,1);
+        content.setLayout(cl);
+        explore.add(btnExplore);
+        retrieve.add(btnRetrieve);
+        create.add(btnCreate);
+        delete.add(btnDelete);
+
+        all.add(explore);
+        all.add(retrieve);
+        all.add(create);
+        all.add(delete);
+
+        content.add(all);
+        cl.show(content,"1");
+
+        f.add(content);
+        f.pack();
+        f.setVisible(true);
         /*
             ClickedEvent implements ActionListener and takes in Menu
             to return the specified action when the button is pressed
@@ -59,22 +86,24 @@ public class MainGUI {
         ClickedEvent deleteClicked = new ClickedEvent(deleteRecipeMenu);
 
         //Adds buttons to the panel
+        /*
         p.add(btnDelete);
         p.add(btnExplore);
         p.add(btnRetrieve);
         p.add(btnInteractive);
         p.add(btnCreate);
-
+        */
         // Attempting to create the TextArea here.
         p.add(new JScrollPane(textWelcome));
         /*
             'Listens' for any clicks on the buttons
          */
+        /*
         btnExplore.addActionListener(exploreClicked);
         btnRetrieve.addActionListener(retrieveClicked);
         btnInteractive.addActionListener(interactiveClicked);
         btnCreate.addActionListener(createClicked);
         btnDelete.addActionListener(deleteClicked);
-        f.repaint();
+        f.repaint();*/
     }
 }
